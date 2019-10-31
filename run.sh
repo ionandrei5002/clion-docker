b41cc296cf7facb9d68b4a31412d1a3b69a43f64
ionandrei5002 <ion_andrei_5002@yahoo.com>
2019-10-31 21:36:54 +0200

    +modif

--- run.sh
+++ run.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
+cache="/home/andrei/dockerbuilds/clion-docker/.cache"
+clion="/home/andrei/dockerbuilds/clion-docker/.CLion2019.2"
+java="/home/andrei/dockerbuilds/clion-docker/.java"
+projects="/home/andrei/CLionProjects"
+
+if [ -d  "$cache" ]
+then 
+    echo "Directory $cache exists."
+else
+    mkdir -p $cache
+    echo "Directory $cache created."
+fi
+
+if [ -d  "$clion" ]
+then 
+    echo "Directory $clion exists."
+else
+    mkdir -p $clion
+    echo "Directory $clion created."
+fi
+
+if [ -d  "$java" ]
+then 
+    echo "Directory $java exists."
+else
+    mkdir -p $java
+    echo "Directory $java created."
+fi
+
+if [ -d  "$projects" ]
+then 
+    echo "Directory $projects exists."
+else
+    mkdir -p $projects
+    echo "Directory $projects created."
+fi
+
 docker run --rm -it -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /home/andrei/dockerbuilds/clion-docker/.cache:/home/andrei/.cache \
-    -v /home/andrei/dockerbuilds/clion-docker/.CLion2019.2:/home/andrei/.CLion2019.2 \
-    -v /home/andrei/dockerbuilds/clion-docker/.java:/home/andrei/.java \
-    -v /home/andrei/CLionProjects:/home/andrei/CLionProjects \
+    -v $cache:/home/andrei/.cache \
+    -v $clion:/home/andrei/.CLion2019.2 \
+    -v $java:/home/andrei/.java \
+    -v $projects:/home/andrei/CLionProjects \
     clion-docker:latest
