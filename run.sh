f088f791432f004b1c5c855b5ff92ada6ba29c18
ionandrei5002 <ion_andrei_5002@yahoo.com>
2019-11-23 22:11:18 +0200

    +patch bspwm java

--- run.sh
+++ run.sh
@@ -43,4 +43,6 @@ docker run --rm -it -e DISPLAY=$DISPLAY \
     -v $clion:/home/andrei/.CLion2019.2 \
     -v $java:/home/andrei/.java \
     -v $projects:/home/andrei/CLionProjects \
+    --env _JAVA_AWT_WM_NONREPARENTING=1 \
+    --env AWT_TOOLKIT=MToolkit \
     clion-docker:latest
